d22d4bf187e40732b9b1af73bb88b000767c6a63
AVanslov <a.buchelnikov99@gmail.com>
2024-03-30 23:15:29 +0100

    add api/users endpoint

--- backend/foodgram_project/foodgram_api/views.py
+++ backend/foodgram_project/foodgram_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+@api_view(['GET', 'POST'])
+def user_list(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    user = User.objects.all()
+    serializer = UserSerializer(user, many=True)
+    return Response(serializer.data)
--- backend/foodgram_project/foodgram_project/urls.py
+++ backend/foodgram_project/foodgram_project/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from foodgram_api.views import (
+    user_list,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api/users/', user_list),
 ]
