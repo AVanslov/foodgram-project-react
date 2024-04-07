953e0332ad1a981828eda5ba5320acc7c71852ef
AVanslov <a.buchelnikov99@gmail.com>
2024-04-07 23:07:47 +0200

    change DEBAG cond in settings

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'foodgram_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG == False:
+if DEBUG is False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
