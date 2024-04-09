71730e9f790626500af55ec81272897c61dc4101
AVanslov <a.buchelnikov99@gmail.com>
2024-04-09 07:32:59 +0200

    moved models, admin

--- backend/foodgram_project/recipes/admin.py
+++ backend/foodgram_project/recipes/admin.py
@@ -1,10 +1,139 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import User
-
+from .models import (
+    Favorite,
+    Follow,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    ShoppingCart,
+    Tag,
+    User,
+)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name')
     list_filter = ('username', 'email')
+    empty_value_display = '-empty-'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'measurement_unit',
+        'recipes_count',
+    )
+    list_filter = ('measurement_unit',)
+    empty_value_display = '-empty-'
+
+    @admin.display(empty_value=None)
+    def recipes_count(self, ingredient):
+        return ingredient.recipes.all().count()
+
+
+class CookongTimeListFilter(admin.SimpleListFilter):
+    title = ('Время приготовления')
+    parameter_name = 'cooking_time'
+
+    def lookups(self, request, model_admin):
+
+        qs = model_admin.get_queryset(request)
+        if qs.filter(
+            cooking_time__range=(0, 10),
+        ).exists():
+            yield ('быстрые', ('быстрее 10 минут'))
+        if qs.filter(
+            cooking_time__range=(10, 30),
+        ).exists():
+            yield ('средние', ('быстрее 30 минут'))
+        else:
+            yield ('долгие', ('более 30 минут'))
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'author',
+        'followers',
+        'cooking_time',
+        'image',
+        'tags',
+        'ingredients'
+    )
+    list_filter = [CookongTimeListFilter]
+    empty_value_display = '-empty-'
+
+    @admin.display(empty_value=None)
+    def followers(self, recipe):
+        return recipe.favorites.all().count()
+
+    @admin.display(empty_value=None)
+    def tags(self, recipe):
+        for tag in recipe.recipe_tags.all():
+            return mark_safe(tag.name)[:50] + '<br>'
+
+    @admin.display(empty_value=None)
+    def ingredients(self, recipe):
+        for ingredient in recipe.recipe_ingredients.all():
+            return (
+                mark_safe(ingredient.name)[:50],
+                mark_safe(ingredient.measurement_unit),
+                mark_safe(ingredient.amount) + '<br>'
+            )
+
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'recipe',
+        'amount'
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'recipe'
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'following'
+    )
+    empty_value_display = '-empty-'
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'recipe'
+    )
     empty_value_display = '-empty-'
--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -36,3 +36,225 @@ class User(AbstractBaseUser):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
         ordering = ('-username',)
+
+
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.db.models import UniqueConstraint
+from django_extensions.validators import HexValidator
+
+User = get_user_model()
+
+NAME_MAX_LENGHT = 200
+SLUG_MAX_LENGHT = 200
+COLOR_MAX_LENGHT = 7
+NUMBER_OF_VISIBLE_CHATACTERS = 15
+
+
+class Tag(models.Model):
+    """Модель таблицы Теги."""
+
+    name = models.CharField(
+        'Заголовок',
+        max_length=NAME_MAX_LENGHT,
+    )
+    color = models.CharField(
+        max_length=COLOR_MAX_LENGHT,
+        default='#ff0000',
+        validators=[HexValidator(length=COLOR_MAX_LENGHT)],
+    )
+    slug = models.SlugField(
+        'Идентификатор',
+        unique=True,
+        max_length=SLUG_MAX_LENGHT,
+        help_text=(
+            'Идентификатор страницы для URL; разрешены символы латиницы, '
+            'цифры, дефис и подчёркивание.'
+        ),
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Ingredient(models.Model):
+    """Модель таблицы Ингрединты."""
+
+    name = models.CharField(
+        'Заголовок',
+        max_length=NAME_MAX_LENGHT,
+    )
+    measurement_unit = models.TextField(
+        'Единицы измерения',
+        default=None,
+    )
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Recipe(models.Model):
+    """Модель таблицы Рецепты."""
+
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиенты',
+        related_name='recipes',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+    )
+    image = models.ImageField(
+        'Фото рецепта',
+        upload_to='image/',
+        null=False,
+        default=None,
+    )
+    name = models.CharField(
+        'Заголовок',
+        max_length=NAME_MAX_LENGHT,
+    )
+    text = models.TextField(
+        'Описание рецепта',
+        default='',
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время приготовления в минутах',
+        help_text='Введите время приготовления в минутах',
+        validators=[MinValueValidator(limit_value=1)],
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('-pub_date',)
+
+
+class RecipeIngredient(models.Model):
+    """
+    Промежуточная таблица Рецептов и ингредиентов
+    с добавочным столбцом Количество.
+    """
+
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиенты в рецепте',
+        related_name='ingredient_in_recipes',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+        related_name='ingredients_in_recipes',
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Мера',
+    )
+
+    class Meta:
+        verbose_name = 'Колличество ингредиента в рецепте'
+        verbose_name_plural = 'Колличество ингредиентов в рецепте'
+
+
+class Follow(models.Model):
+    """
+    Модель для связи между автором и подписчиком.
+    """
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followers',
+        verbose_name='Подписчик',
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='authors',
+        verbose_name='автор',
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'following'], name='unique_follow'
+            ),
+        ]
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+
+
+class UserRecipeAbstractModel(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_recipe',
+            ),
+        ]
+        abstract = True
+
+
+class Favorite(UserRecipeAbstractModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='favorites',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name='Избранный рецепт',
+    )
+
+    class Meta:
+
+        verbose_name = 'Избранный рецепт'
+        verbose_name_plural = 'Избранные рецепты'
+
+
+class ShoppingCart(UserRecipeAbstractModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='purchases',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='purchases',
+        verbose_name='Рецепт в списке покупок',
+    )
+
+    class Meta:
+
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
